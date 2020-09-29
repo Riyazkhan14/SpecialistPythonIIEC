42948a70a424a7ca8a7c5ec72bd6b7eeeed2db6d
Riyazuddin <21050074+Riyazkhan14@users.noreply.github.com>
2020-09-29 21:11:56 +0530

    WEB
    Docker Run Files Task 2 IIEC Rise

---
+++ Docker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+print("content-type: text/html")
+print()
+
+import subprocess as sp
+import cgi
+
+form = cgi.FieldStorage()
+
+
+osname = form.getvalue("x")
+osimage = form.getvalue("i")
+
+cmd = "sudo docker run -d -i -t --name {0} {1}".format(osname,osimage)
+
+output = sp.getstatusoutput(cmd)
+
+status = output[0]
+out = output[1]
+
+if status == 0:
+  print("Your new OS launched {} ..".format(osname))
+else:
+  print("Here is some error : {}".format(out))
